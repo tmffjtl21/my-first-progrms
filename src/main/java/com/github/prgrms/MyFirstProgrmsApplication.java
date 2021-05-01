8c77293443bbcc77e47ff475adc717bc17d2c48d
LEETAEJIN <tmffjtl21@osstem.com>
2021-05-02 00:18:20 +0900

    initial commit

---
+++ src/main/java/com/github/prgrms/MyFirstProgrmsApplication.java
@@ -0,0 +1,13 @@
+package com.github.prgrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProgrmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstProgrmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/prgrms/MyFirstProgrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.prgrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProgrmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
